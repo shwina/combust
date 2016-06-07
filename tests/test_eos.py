a7954dc793e9f41e6a0a6402c22effe87dd392ed
Ashwin Srinath <atrikut@clemson.edu>
2016-06-07 12:45:15 -0400

    split eos tests

--- tests/test_eos.py
+++ tests/test_eos.py
@@ -4,16 +4,17 @@ from combust.models.eos import IdealGasModel
 import numpy as np
 from numpy.testing import assert_allclose
 
-def test_ideal_gas_model():
-    cp = np.array([1005., 0., 1005.])
-    cv = np.array([718., 0., 718.])
-    R = cp-cv
+def test_ideal_gas_single_species():
     rho = np.ones([5, 5], dtype=np.float64)*1.187
-    y = np.zeros([5, 5, 3], dtype=np.float64)
-    y[:, :, 0] = 1.0
     u = np.zeros([5, 5], dtype=np.float64)
     v = np.zeros([5, 5], dtype=np.float64)
     E = rho*718.*298. +  0.5*rho*(u**2 + v**2)
+    y = np.zeros([5, 5, 3], dtype=np.float64)
+
+    cp = np.array([1005., 0., 1005.])
+    cv = np.array([718., 0., 718.])
+    R = cp-cv
+    y[:, :, 0] = 1.0
 
     eos = IdealGasModel(cp, cv, R)
     T, P = eos.get_temperature_and_pressure(rho, y, E, u, v)
@@ -30,6 +31,13 @@ def test_ideal_gas_model():
     T, P = eos.get_temperature_and_pressure(rho, y, E, u, v)
     assert_allclose(T, 298.)
     assert_allclose(P, 101519.362)
+
+def test_ideal_gas_multiple_species():
+    rho = np.ones([5, 5], dtype=np.float64)*1.187
+    u = np.zeros([5, 5], dtype=np.float64)
+    v = np.zeros([5, 5], dtype=np.float64)
+    E = rho*718.*298. +  0.5*rho*(u**2 + v**2)
+    y = np.zeros([5, 5, 3], dtype=np.float64)
 
     cp = np.array([1005., 1005., 1005.])
     cv = np.array([718., 718., 718.])
